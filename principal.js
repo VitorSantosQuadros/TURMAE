2a701df82fd4e2616caace6b48ec2304b1ffacda
Vitor Quadros <107114183+VitorSantosQuadros@users.noreply.github.com>
2022-11-21 17:46:01 -0300

    Update principal.js

--- principal.js
+++ principal.js
@@ -1,5 +1,5 @@
   var botao = document.querySelector("botao")
-                botaoAdicionar.addEvenListener("click",function(event){
+                botao.addEvenListener("click",function(event){
                 event.preventDefault();
                     console.log("Oi eu sou o Botão e eu fui clicado")
                 
