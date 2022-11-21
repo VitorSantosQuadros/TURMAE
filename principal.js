8f9bf57e79af925f963531aa4724f5bc54fef304
Vitor Quadros <107114183+VitorSantosQuadros@users.noreply.github.com>
2022-11-21 17:03:01 -0300

    Update principal.js

--- principal.js
+++ principal.js
@@ -1,1 +1,7 @@
-
+// Javascript
+var form = document.querySelector('.adicionar-tarefa');
+var botao = document.querySelector('#botao-adicionar');
+botao.addEventListener("click", function(){
+    console.log(form.tarefa.value);
+    //Código para adicionar na lista de tarefas
+});
