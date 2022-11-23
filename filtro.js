b74034183f4d6ff13d1c73a3b179c71fe8dcd61b
Vitor Quadros <107114183+VitorSantosQuadros@users.noreply.github.com>
2022-11-23 18:55:43 -0300

    Create filtro.js

---
+++ filtro.js
@@ -0,0 +1,12 @@
+var campoFiltro = document.querySelector(#filtrar)
+
+campoFiltro.addEvenListener("input", function(){
+console.log(this.value)
+var pacinete =document.querySelectorAll("clientes")
+for ( var i = 0; i < clientes.length ; i++){
+var clientes = clientes[i]
+var tdNome = cliente.querySelector("info-nome")
+var nome = cliente.textContent
+}
+  
+})
